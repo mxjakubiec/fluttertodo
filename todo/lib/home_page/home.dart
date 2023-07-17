5245840ba53691463441d3782aa8181e5563df2d
mxjakubiec <maksymilian.jakubiec@microsoft.wsei.edu.pl>
2023-07-17 14:45:26 +0200

    home.dart class + main.dart(app bar & colors)

---
+++ todo/lib/home_page/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage ({super.key})
+
+  @override 
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[200],
+      appBar: AppBar(
+        title: Text('TO DO'),
+      ),
+
+    );
+  }
+}
